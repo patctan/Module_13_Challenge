70386bff0272b547336288b9f1bdff09b162072e
Bitaw <stickynotes5@yahoo.com>
2022-12-12 16:31:44 -0600

    Added routes to tag-routes.js

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,27 +1,54 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  Tag.findAll({ include: [Product] })
+    .then((tagData) => {
+      res.json(tagData);
+    })
+    .catch((err) => console.log(err));
   // find all tags
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
+  Tag.findOne({ where: { id: req.params.id } }).then((tagData) => {
+    res.json(tagData.catch((err) => console.log(err)));
+  });
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
+  Tag.create(req.bod).then((tagData) => {
+    res.json(tagData);
+  });
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
+  Tag.update(req.body, { where: { id: req.params.id } })
+    .then((tagData) => {
+      res.json(tagData);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tagData) => {
+      res.json(tagData);
+    })
+    .catch((err) => res.json(err));
   // delete on tag by its `id` value
 });
 
